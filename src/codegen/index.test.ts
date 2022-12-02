7070150dc2a2a455056cb2d96b807c48a838929a
Felix Gnass <fgnass@gmail.com>
2022-12-02 10:06:24 +0100

    chore: clean up tests

---
+++ src/codegen/index.test.ts
@@ -0,0 +1,92 @@
+import * as path from "path";
+import { generateSource } from "./index";
+
+/**
+ * Generate an API from a releative path and convert it into a single line.
+ */
+async function generate(file: string, opts = {}) {
+  const spec = path.join(__dirname, file);
+  const src = await generateSource(spec, opts);
+  return src.replace(/\s+/g, " ");
+}
+
+describe("generateSource", () => {
+  it("should generate the same api twice", async () => {
+    const spec = "/../../demo/petstore.json";
+    const src1 = await generate(spec);
+    const src2 = await generate(spec);
+    expect(src1).toBe(src2);
+  });
+
+  it("should handle enums as union types", async () => {
+    const src = await generate("/../../demo/petstore.json");
+    expect(src).toContain(`export type Option = ("one" | "two" | "three")[];`);
+  });
+
+  it("should handle properties both inside and outside of allOf", async () => {
+    const src = await generate("/__fixtures__/allOf.json");
+    expect(src).toContain(
+      "export type Circle = Shape & { radius?: number; } & { circumference?: number; };"
+    );
+  });
+
+  it("should handle application/geo+json", async () => {
+    const src = await generate("/__fixtures__/geojson.json");
+    expect(src).toContain(
+      'return oazapfts.fetchJson<{ status: 200; data: FeatureCollection; }>("/geojson", { ...opts });'
+    );
+  });
+
+  it("should generate an api using fetchBlob", async () => {
+    const src = await generate("/__fixtures__/binary.json");
+    expect(src).toContain(
+      "return oazapfts.fetchBlob<{ status: 200; data: Blob; }>(`/file/${encodeURIComponent(fileId)}/download`, { ...opts });"
+    );
+  });
+
+  it("should generate an api with literal type set to const value", async () => {
+    const src = await generate("/__fixtures__/const.json");
+    expect(src).toContain(`export type Shape = "circle";`);
+  });
+});
+
+describe("useEnumType", () => {
+  let src: string;
+
+  beforeAll(async () => {
+    src = await generate("/../../demo/petstore.json", { useEnumType: true });
+  });
+
+  it("should create string enums", () => {
+    expect(src).toContain(
+      `export enum Status { Available = "Available", Pending = "Pending", Sold = "Sold" }`
+    );
+  });
+
+  it("should create number enums", () => {
+    expect(src).toContain(`export enum Size { P = 0, M = 1, G = 2 }`);
+  });
+
+  it("should handle values with the same name", () => {
+    expect(src).toContain(
+      `export enum Status2 { Placed = "Placed", Approved = "Approved", Delivered = "Delivered" }`
+    );
+  });
+
+  it("should avoid name conflicts between types and enums", () => {
+    // Type Category is defined as `Category`
+    expect(src).toContain(
+      `export type Category = { id?: number; name?: string; };`
+    );
+
+    // Enum Category is also defined as `Category` which would be a conflict to type `Category`
+    expect(src).not.toContain(
+      `export enum Category { Rich = "Rich", Wealthy = "Wealthy", Poor = "Poor" }`
+    );
+
+    // Enum Category is defined as `Category2` to avoid name conflict with type Category
+    expect(src).toContain(
+      `export enum Category2 { Rich = "Rich", Wealthy = "Wealthy", Poor = "Poor" }`
+    );
+  });
+});
--- src/codegen/index.ts
+++ src/codegen/index.ts
@@ -3,7 +3,7 @@ import ApiGenerator from "./generate";
 import ts from "typescript";
 import SwaggerParser from "@apidevtools/swagger-parser";
 import converter from "swagger2openapi";
-import { OpenAPIV3 } from "openapi-types";
+import { OpenAPI, OpenAPIV3 } from "openapi-types";
 
 export { cg };
 
@@ -16,30 +16,42 @@ export type Opts = {
 };
 
 export function generateAst(
-  spec: OpenAPIV3.Document,
+  doc: OpenAPIV3.Document,
   opts: Opts,
   isConverted: boolean
 ) {
-  return new ApiGenerator(spec, opts, isConverted).generateApi();
+  return new ApiGenerator(doc, opts, isConverted).generateApi();
 }
 
 export function printAst(ast: ts.SourceFile) {
   return cg.printFile(ast);
 }
 
-export async function generateSource(spec: string, opts: Opts) {
-  let v3Doc;
-  const doc = await SwaggerParser.bundle(spec);
-  const isOpenApiV3 = "openapi" in doc && doc.openapi.startsWith("3");
-  if (isOpenApiV3) {
-    v3Doc = doc as OpenAPIV3.Document;
-  } else {
-    const result = await converter.convertObj(doc, {});
-    v3Doc = result.openapi as OpenAPIV3.Document;
-  }
-  const ast = generateAst(v3Doc, opts, !isOpenApiV3);
-  const { title, version } = v3Doc.info;
+export async function generateSource(spec: string, opts: Opts = {}) {
+  var { doc, isConverted } = await parseSpec(spec);
+  const ast = generateAst(doc, opts, isConverted);
+  const { title, version } = doc.info;
   const preamble = ["$&", title, version].filter(Boolean).join("\n * ");
   const src = printAst(ast);
   return src.replace(/^\/\*\*/, preamble);
+}
+
+function isOpenApiV3(doc: OpenAPI.Document): doc is OpenAPIV3.Document {
+  return "openapi" in doc && doc.openapi.startsWith("3");
+}
+
+export async function parseSpec(spec: string) {
+  const doc = await SwaggerParser.bundle(spec);
+  if (isOpenApiV3(doc)) {
+    return {
+      doc,
+      isConverted: false,
+    };
+  } else {
+    const converted = await converter.convertObj(doc, {});
+    return {
+      doc: converted.openapi as OpenAPIV3.Document,
+      isConverted: true,
+    };
+  }
 }
