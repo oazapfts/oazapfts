ad26afd0f40f29cea0a8e8d9a81676ec26915a08
bdm-k <kokusyunn@gmail.com>
2023-08-30 18:18:13 +0900

    Fix a small mistake in getRefName comment

--- src/codegen/generate.ts
+++ src/codegen/generate.ts
@@ -137,7 +137,7 @@ function getRefBasename(ref: string) {
 
 /**
  * Returns a name for the given ref that can be used as basis for a type
- * alias. This usually is the baseName, unless the ref ends with a number,
+ * alias. This usually is the baseName, unless the ref starts with a number,
  * in which case the whole ref is returned, with slashes turned into
  * underscores.
  */
