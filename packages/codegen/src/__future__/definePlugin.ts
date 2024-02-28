d2ad7e8cdd07e7e38f429bb87d445de1d2926586
Alex Riviere <alex.riviere@nexcortech.com>
2024-02-28 14:22:22 -0500

    Change to use Tapable

--- packages/codegen/src/__future__/definePlugin.ts
+++ packages/codegen/src/__future__/definePlugin.ts
@@ -1,175 +1,233 @@
-import { OpenAPIV3 } from "openapi-types";
+import {OpenAPIV3} from "openapi-types";
 import {PathOrFileDescriptor} from "node:fs";
-import { type SchemaObject } from "../generate";
+import {type SchemaObject} from "../generate";
 import ts from "typescript";
-import { Opts } from "../index";
+import {Opts} from "../index";
 import minimist from "minimist";
+import {AsyncSeriesWaterfallHook, AsyncSeriesHook} from "tapable";
 
-export type OazapftsCodegenPlugin = {
-  /**
-   * identifier for this plugin
-   */
-  name?: string;
-  /**
-   * Additional command line arguments that can be specified
-   * This will be parsed after all plugins have been loaded.
-   */
-  options?: {
-    /**
-     * The new arguments to be merged into the options object.
-     * Cannot override existing option names.
-     */
-    args: minimist.Opts;
-    /**
-     * Description for the plugin options.
-     */
-    description: string;
-    /**
-     * Validate if the options you have are valid or not.
-     * should throw an error if the options are not valid.
-     * @param opts - the parsed options object.
-     */
-    validator?(opts: Opts): void;
-  };
+namespace OazapftsPlugin {
 
-  /**
-   * Called after the OAS document has been downloaded and before AST transform begins.
-   * Can be async.
-   * @param options - command line options. will include values defined by plugin.
-   * @param document - the full OAS document.
-   */
-  initialize?(
-    options: Opts,
-    document: OpenAPIV3.Document,
-  ): void | Promise<void>;
+    export class CliContext {
+        /**
+         * Parsed cli Arguments. Starts off undefined until arguments are parsed.
+         */
+        public options?: minimist.ParsedArgs;
+        /**
+         * Definition of cli Arguments
+         */
+        public args: minimist.Opts = {
+            string: [],
+            boolean: [],
+            alias: {},
+            default: {},
+            unknown: () => true,
+            "--": true,
+            stopEarly: false,
+        };
+        /**
+         * Used to display information about the cli.
+         * Append your cli argument descriptions here.
+         */
+        public description: string[] = []
+    }
 
-  /**
-   * Modify the `defaults` value definition in the head of the new TS document
-   * Can be async
-   * @param defaults - the AST of the `defaults` definition
-   * @param fullTree - the full AST tree of the document
-   * @param document - the full OAS document
-   */
-  modifyDefaults?(
-    defaults: ts.VariableDeclaration,
-    fullTree: ts.SourceFile,
-    document: OpenAPIV3.Document,
-  ): void | Promise<void>;
+    export class Hooks {
+        /**
+         * Called before anything is run.
+         * Allows you to add argument parsing options to the cli.
+         */
+        public cliArgs = new AsyncSeriesHook<[CliContext]>(['context'], 'cliArgs');
+        /**
+         * Optionally validate any CLI arguments your plugin is looking for
+         */
+        public cliValidateArgs = new AsyncSeriesHook<[CliContext]>(['context'], 'cliValidateArgs');
+        /**
+         * Modify the context before we fetch an OpenAPIV3.Document
+         */
+        public oasPrefetch = new AsyncSeriesHook<[CliContext]>(['context'], 'oasPrefetch');
+        /**
+         * actually fetching the OpenAPIV3.Document
+         */
+        public oasFetch = new AsyncSeriesWaterfallHook<[
+            /** @description the full OAS Document*/
+                OpenAPIV3.Document | null,
+            /** @description The current cli context options */
+            OazapftsPlugin.CliContext
+        ]>(['document', 'context'])
+        /**
+         * Modify the `defaults` value definition in the head of the new TS document
+         */
+        public tsModifyDefaults = new AsyncSeriesHook<[
+            /** @description the AST of the `defaults` definition */
+            ts.VariableDeclaration,
+            /** @description the full AST tree of the document */
+            ts.SourceFile,
+            /** @description the full OAS document */
+            OpenAPIV3.Document,
+            /** @description The current cli context options */
+            OazapftsPlugin.CliContext
+        ]>(
+            ['defaults', 'fullTree', 'document', 'context'],
+            'tsModifyDefaults'
+        );
+        /**
+         * Modify the `servers` value definition in the head of the new TS document
+         */
+        public tsModifyServers = new AsyncSeriesHook<[
+            /** @description the AST of the `servers` definition */
+            ts.VariableDeclaration,
+            /** @description  the full AST tree of the document */
+            ts.SourceFile,
+            /** @description  the full OAS document */
+            OpenAPIV3.Document,
+            /** @description The current cli context options */
+            OazapftsPlugin.CliContext
+        ]>(
+            ['servers', 'fullTree', 'document', 'context'],
+            'tsModifyServers',
+        );
 
-  /**
-   * Modify the `servers` value definition in the head of the new TS document
-   * Can be async
-   * @param servers - the AST of the `servers` definition
-   * @param fullTree - the full AST tree of the document
-   * @param document - the full OAS document
-   */
-  modifyServers?(
-    servers: ts.VariableDeclaration,
-    fullTree: ts.SourceFile,
-    document: OpenAPIV3.Document,
-  ): void | Promise<void>;
+        /**
+         * Run before an OAS endpoint is converted to an AST node
+         */
+        public convertEndpointBefore = new AsyncSeriesHook<[
+            /** @description The current endpoint Object from the OAS document */
+            OpenAPIV3.OperationObject,
+            /** @description the full AST tree */
+            ts.SourceFile,
+            /** @description the full OAS document */
+            OpenAPIV3.Document,
+            /** @description The current cli context options */
+            OazapftsPlugin.CliContext,
+        ]>(
+            ['schema', 'fullTree', 'document', 'context'],
+            'convertEndpointBefore'
+        );
 
-  /**
-   * Run before an OAS endpoint is converted to an AST node
-   * Can be async
-   * @param schema - the current endpoint Object from the OAS document
-   * @param fullTree - the full AST tree
-   * @param document - the full OAS document
-   */
-  beforeConvertEndpoint?(
-    schema: OpenAPIV3.OperationObject,
-    fullTree: ts.SourceFile,
-    document: OpenAPIV3.Document,
-  ): void | Promise<void>;
+        /**
+         * converts an endpoint to an AST node
+         */
+        public convertEndpoint = new AsyncSeriesWaterfallHook<[
+            /** @description the AST node for the function definition */
+                ts.FunctionDeclaration | null,
+            /** @description the current endpoint Object from the OAS document */
+            OpenAPIV3.OperationObject,
+            /** @description the full AST tree*/
+            ts.SourceFile,
+            /** @description the full OAS document*/
+            OpenAPIV3.Document,
+            /** @description The current cli context options */
+            OazapftsPlugin.CliContext,
+        ]>(
+            ['functionTree', 'schema', 'fullTree', 'document', 'context'],
+            'convertEndpoint'
+        );
 
-  /**
-   * Run after an OAS endpoint is converted to an AST node
-   * Can be async
-   * @param schema - the current endpoint Object from the OAS document
-   * @param functionTree - the AST node for the function definition
-   * @param fullTree - the full AST tree
-   * @param document - the full OAS document
-   */
-  afterConvertEndpoint?(
-    schema: OpenAPIV3.OperationObject,
-    functionTree: ts.FunctionDeclaration,
-    fullTree: ts.SourceFile,
-    document: OpenAPIV3.Document,
-  ): void | Promise<void>;
+        /**
+         * Run before an OAS schema is converted to an AST node
+         */
+        public convertSchemaBefore = new AsyncSeriesHook<[
+            /** @description the current OAS Schema about to be converted */
+                SchemaObject | OpenAPIV3.ReferenceObject,
+            /** @description the full AST tree */
+            ts.SourceFile,
+            /** @description the full OAS document */
+            OpenAPIV3.Document,
+            /** @description The current cli context options */
+            OazapftsPlugin.CliContext,
+        ]>(
+            ['schema', 'fullTree', 'document', 'context'],
+            'convertSchemaBefore'
+        );
 
-  /**
-   * Run before an OAS schema is converted to an AST node
-   * Can be async
-   * @param schema - the current OAS Schema about to be converted
-   * @param fullTree - the full AST tree
-   * @param document - the full OAS document
-   */
-  beforeConvertSchema?(
-    schema: SchemaObject | OpenAPIV3.ReferenceObject,
-    fullTree: ts.SourceFile,
-    document: OpenAPIV3.Document,
-  ): void | Promise<void>;
+        /**
+         * converts an OAS schema to an AST node
+         */
+        public convertSchema = new AsyncSeriesWaterfallHook<[
+            /** @description the AST Type Definition */
+            ts.TypeNode,
+            /** the current OAS Schema that was converted */
+                SchemaObject | OpenAPIV3.ReferenceObject,
+            /** @description the full AST tree */
+            ts.SourceFile,
+            /** @description the full OAS document */
+            OpenAPIV3.Document,
+            /** @description The current cli context options */
+            OazapftsPlugin.CliContext,
+        ]>(
+            ['typeTree', 'schema', 'fullTree', 'document', 'context'],
+            'convertSchema'
+        );
 
-  /**
-   * Run after an OAS schema is converted to an AST node
-   * Can be async
-   * @param schema - the current OAS Schema that was converted
-   * @param typeTree - the AST Type Definition
-   * @param fullTree - the full AST tree
-   * @param document - the full OAS document
-   */
-  afterConvertSchema?(
-    schema: SchemaObject | OpenAPIV3.ReferenceObject,
-    typeTree: ts.TypeNode,
-    fullTree: ts.SourceFile,
-    document: OpenAPIV3.Document,
-  ): void | Promise<void>;
+        /**
+         * Runs after the full AST tree has been created, but before the ts file contents have been generated
+         */
+        public astGenerated = new AsyncSeriesHook<[
+            /** @description the full AST tree */
+            ts.SourceFile,
+            /** @description the full OAS document */
+            OpenAPIV3.Document,
+            /** @description The current cli context options */
+            OazapftsPlugin.CliContext,
+        ]>(
+            ['fullTree', 'document', 'context'],
+            'astGenerated'
+        );
 
-  /**
-   * Runs after the full AST tree has been created, but before the ts file contents have been generated
-   * Can be async
-   * @param fullTree - the full AST tree
-   * @param document - the full OAS document
-   */
-  afterASTGenerate?(
-    fullTree: ts.SourceFile,
-    document: OpenAPIV3.Document,
-  ): void | Promise<void>;
+        /**
+         * Runs after the TS file contents have been generated, but before writing to file.
+         */
+        public writeFileBefore = new AsyncSeriesHook<[
+            /** @description path to the file to write to */
+            PathOrFileDescriptor,
+            /** @description The TS file contents as a string. */
+            string,
+            /** @description the full OAS document */
+            OpenAPIV3.Document,
+            /** @description The current cli context options */
+            OazapftsPlugin.CliContext,
+        ]>(
+            ['fileName', 'fileContents', 'document', 'context'],
+            'writeFileBefore'
+        );
 
-  /**
-   * Runs after the TS file contents have been generated, but before writing to file.
-   * Can be async
-   * @param fileName - path to the file to write to
-   * @param fileContents - The TS file contents as a string.
-   * @param document - the full OAS document.
-   */
-  beforeWriteToFile?(
-    fileName: PathOrFileDescriptor,
-    fileContents: string,
-    document: OpenAPIV3.Document,
-  ): void | Promise<void>;
+        /**
+         * Runs after the TS file has been written to. Allows for... cleanup?
+         * @param fileName - path of the file that was written to
+         * @param fileContents - The TS file contents as a string.
+         * @param document - the full OAS document.
+         */
+        public writeFile = new AsyncSeriesHook<[
+            /** @description path to the file to write to */
+            PathOrFileDescriptor,
+            /** @description The TS file contents as a string. */
+            string,
+            /** @description the full OAS document */
+            OpenAPIV3.Document,
+            /** @description The current cli context options */
+            OazapftsPlugin.CliContext,
+        ]>(
+            ['fileName', 'fileContents', 'document', 'context'],
+            'writeFileBefore'
+        );
+    }
 
-  /**
-   * Runs after the TS file has been written to. Allows for... cleanup?
-   * @param fileName - path of the file that was written to
-   * @param fileContents - The TS file contents as a string.
-   * @param document - the full OAS document.
-   */
-  afterWriteToFile?(
-    fileName: PathOrFileDescriptor,
-    fileContents: string,
-    document: OpenAPIV3.Document,
-  ): void | Promise<void>;
-};
+    export type Callback = (hooks: OazapftsPlugin.Hooks) => void;
 
+}
+
+
+
+
+export const __globalHooks = new OazapftsPlugin.Hooks();
 
 export function defineOazapftsPlugin(
-  plugin: OazapftsCodegenPlugin,
-): OazapftsCodegenPlugin {
-  // Do some checks to ensure that we have a valid plugin (if there are required params)
-  if(typeof plugin !== "object" || Array.isArray(plugin) || plugin === null ){
-    throw new Error("Plugin is not an object.")
-  }
-  // Maybe we do the registration right here? force module developers to use this function?
-  return plugin;
+    plugin: OazapftsPlugin.Callback,
+): void {
+    // Do some checks to ensure that we have a valid plugin (if there are required params)
+    if (typeof plugin !== "function" || plugin.length < 1) {
+        throw new Error("Plugin must be a function and accept 1 argument");
+    }
+    plugin(__globalHooks);
 }
