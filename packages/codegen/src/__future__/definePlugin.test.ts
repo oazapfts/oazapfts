17ecb1ef6b164969869ffe98fd3cd3640fb41217
Alex Riviere <alex.riviere@nexcortech.com>
2024-02-29 08:33:24 -0500

    prettier

--- packages/codegen/src/__future__/definePlugin.test.ts
+++ packages/codegen/src/__future__/definePlugin.test.ts
@@ -1,21 +1,21 @@
-import {describe, it, expect, vi, beforeEach} from "vitest";
+import { describe, it, expect, vi, beforeEach } from "vitest";
 import {
   __globalHooks,
-    __updateGlobalHooks,
-    defineOazapftsPlugin,
-    CliContext,
-    Hooks,
+  __updateGlobalHooks,
+  defineOazapftsPlugin,
+  CliContext,
+  Hooks,
 } from "./definePlugin";
 
 describe("defineOazapftsPlugin", () => {
-  beforeEach(()=>{
-    __updateGlobalHooks(new Hooks())
-  })
+  beforeEach(() => {
+    __updateGlobalHooks(new Hooks());
+  });
   it("should call hooked method cliArgs", async () => {
     const mockFunction = vi.fn<unknown[], void>();
     expect(__globalHooks.cliArgs.isUsed()).toBeFalsy();
-    await defineOazapftsPlugin(async (hooks)=>{
-      hooks.cliArgs.tap('mockFunction', mockFunction);
+    await defineOazapftsPlugin(async (hooks) => {
+      hooks.cliArgs.tap("mockFunction", mockFunction);
     });
     await __globalHooks.cliArgs.promise(new CliContext());
     expect(mockFunction).toBeCalled();
@@ -24,8 +24,8 @@ describe("defineOazapftsPlugin", () => {
   it("should call cliArgs again", async () => {
     const mockFunction = vi.fn<unknown[], void>();
     expect(__globalHooks.cliArgs.isUsed()).toBeFalsy();
-    await defineOazapftsPlugin(async (hooks)=>{
-      hooks.cliArgs.tap('mockFunction', mockFunction);
+    await defineOazapftsPlugin(async (hooks) => {
+      hooks.cliArgs.tap("mockFunction", mockFunction);
     });
     await __globalHooks.cliArgs.promise(new CliContext());
     expect(mockFunction).toBeCalled();
@@ -33,13 +33,12 @@ describe("defineOazapftsPlugin", () => {
 
   it("should call hooked method cliValidateArgs", async () => {
     const mockFunction = vi.fn<unknown[], void>();
-    await defineOazapftsPlugin(async (hooks)=>{
-      hooks.cliValidateArgs.tap('mockFunction', mockFunction);
+    await defineOazapftsPlugin(async (hooks) => {
+      hooks.cliValidateArgs.tap("mockFunction", mockFunction);
     });
 
     await __globalHooks.cliValidateArgs.promise(new CliContext());
 
     expect(mockFunction).toBeCalled();
-
   });
 });
