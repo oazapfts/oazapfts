3535b1b64e3ea4c155628825d8a9d6f92573cbcc
Alex Riviere <fimion@users.noreply.github.com>
2024-03-01 04:00:23 -0500

    future: plugin system
    * first draft of plugin definition
    
    * update with basic tests
    
    * some updates based on comment
    
    * some updates based on comment
    
    * Change to use Tapable
    
    * add some tests
    
    * more tests and updating stuff
    
    * prettier
    
    * prettier
    
    ---------
    
    Co-authored-by: Xiphe <github@xiphe.net>

---
+++ packages/codegen/src/__future__/definePlugin.test.ts
@@ -0,0 +1,44 @@
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import {
+  __globalHooks,
+  __updateGlobalHooks,
+  defineOazapftsPlugin,
+  CliContext,
+  Hooks,
+} from "./definePlugin";
+
+describe("defineOazapftsPlugin", () => {
+  beforeEach(() => {
+    __updateGlobalHooks(new Hooks());
+  });
+  it("should call hooked method cliArgs", async () => {
+    const mockFunction = vi.fn<unknown[], void>();
+    expect(__globalHooks.cliArgs.isUsed()).toBeFalsy();
+    await defineOazapftsPlugin(async (hooks) => {
+      hooks.cliArgs.tap("mockFunction", mockFunction);
+    });
+    await __globalHooks.cliArgs.promise(new CliContext());
+    expect(mockFunction).toBeCalled();
+  });
+
+  it("should call cliArgs again", async () => {
+    const mockFunction = vi.fn<unknown[], void>();
+    expect(__globalHooks.cliArgs.isUsed()).toBeFalsy();
+    await defineOazapftsPlugin(async (hooks) => {
+      hooks.cliArgs.tap("mockFunction", mockFunction);
+    });
+    await __globalHooks.cliArgs.promise(new CliContext());
+    expect(mockFunction).toBeCalled();
+  });
+
+  it("should call hooked method cliValidateArgs", async () => {
+    const mockFunction = vi.fn<unknown[], void>();
+    await defineOazapftsPlugin(async (hooks) => {
+      hooks.cliValidateArgs.tap("mockFunction", mockFunction);
+    });
+
+    await __globalHooks.cliValidateArgs.promise(new CliContext());
+
+    expect(mockFunction).toBeCalled();
+  });
+});
